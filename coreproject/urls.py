19a16b68623b64ed24721313701610c24d200578
IFEANYI OMEATA <omeatai@gmail.com>
2022-06-22 09:05:58 +0100

    Create Serializer for the user object

--- coreproject/urls.py
+++ coreproject/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
+    # path(
+    #     'api/docs/',
+    #     SpectacularSwaggerView.as_view(url_name='api-schema'),
+    #     name='api-docs',
+    # ),
+    path('api/user/', include('user.urls')),
 ]
